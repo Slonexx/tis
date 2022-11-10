b006e16e55578bd65d05265f0d76bea7e15519de
Slonexxx <21moriarty21@mail.ru>
2022-11-10 10:54:37 +0600

    new route to kassa id and id department

--- resources/views/widget/salesreturn.blade.php
+++ resources/views/widget/salesreturn.blade.php
@@ -1,4 +1,7 @@
 
+
+
+
 @extends('widget.widget')
 
 @section('content')
@@ -14,27 +17,48 @@
             const receivedMessage = event.data;
             $('#workerAccess_yes').show();
             if (receivedMessage.name === 'Open') {
-
-                console.log('Global_object_Id = ' + receivedMessage.objectId );
                 Global_object_Id = receivedMessage.objectId;
-
-                var sendingMessage = {
-                    name: "OpenFeedback",
-                    correlationId: receivedMessage.messageId
+                let params = {
+                    accountId: Global_accountId,
+                    entity_type: entity_type,
+                    objectId: Global_object_Id,
                 };
-                logSendingMessage(sendingMessage);
-                hostWindow.postMessage(sendingMessage, '*');
+                let url = 'https://smarttis.kz/widget/InfoAttributes/';
+                let final = url + formatParams(params);
 
                 const xmlHttpRequest = new XMLHttpRequest();
                 xmlHttpRequest.addEventListener("load", function() {
+                    var json = JSON.parse(this.responseText);
+                    console.log(json.ticket_id);
+                    let btnF = window.document.getElementById('btnF')
 
+                    if (json.ticket_id == null){
+                        btnF.innerText = 'Возврат';
+                    } else {
+                        btnF.innerText = 'Действие с чеком';
+                        window.document.getElementById('messageGoodAlert').style.display = 'block'
+                        window.document.getElementById("messageGoodAlert").innerText = "Чек уже создан. Фискальный номер:  " + json.ticket_id
+                    }
+
+                    var sendingMessage = {
+                        name: "OpenFeedback",
+                        correlationId: receivedMessage.messageId
+                    };
+                    hostWindow.postMessage(sendingMessage, '*');
                 });
-                xmlHttpRequest.open("GET", "");
+                xmlHttpRequest.open("GET", final);
                 xmlHttpRequest.send();
             }
 
         });
-
+        function formatParams(params) {
+            return "?" + Object
+                .keys(params)
+                .map(function (key) {
+                    return key + "=" + encodeURIComponent(params[key])
+                })
+                .join("&")
+        }
         function fiscalization(){
 
             Global_messageId++;
@@ -58,62 +82,24 @@
             console.log("← Sending" + " message: " + messageAsString);
         }
 
-
-
     </script>
 
 
     <div class="row gradient rounded p-2">
-        <div class="col-10">
+        <div class="col-6">
             <div class="mx-2"> <img src="https://test.ukassa.kz/_nuxt/img/d2b49fb.svg" width="90%"   alt=""> </div>
         </div>
         <div class="col-2 ">
-            <button type="submit" onclick="" class="myButton btn "> <i class="fa-solid fa-arrow-rotate-right"></i> </button>
+
         </div>
     </div>
-    <div id="workerAccess_yes" class="row mt-2 rounded bg-white" style="display:none;">
-        <div class="col-1"></div>
-        <button onclick="fiscalization()" class="col-10 btn btn-warning text-black rounded-pill"> Возврат </button>
+
+    <div id="messageGoodAlert" class=" mt-1 mx-3 p-2 alert alert-success text-center " style="display: none; font-size: 12px; margin-bottom: 5px !important;">    </div>
+
+    <div id="workerAccess_yes" class="mt-1 mx-4 text-center" style="display:none;">
+        <button id="btnF" onclick="fiscalization()" class=" btn btn-warning text-white rounded-pill">  </button>
     </div>
-    <div id="workerAccess_no" class="row mt-2 rounded bg-white" style="display: none">
-        <div class="col-1"></div>
-        <div class="col-10">
-            <div class="text-center">
-                <div class="p-3 mb-2 bg-danger text-white">
-                   <span class="s-min-10">
-                        У вас нет доступа к данному виджету, сообщите администратору, чтоб он вам предоставил доступ
-                        <i class="fa-solid fa-ban "></i>
-                    </span>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
 
 @endsection
-
-<style>
-    .myButton {
-        box-shadow: 0px 4px 5px 0px #5d5d5d !important;
-        background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%); !important;
-        color: white !important;
-        border-radius:50px !important;
-        display:inline-block !important;
-        cursor:pointer !important;
-        padding:5px 5px !important;
-        text-decoration:none !important;
-    }
-    .myButton:hover {
-        filter: invert(1);
-
-        color: #111111 !important;
-    }
-    .myButton:active {
-        position: relative !important;
-        top: 1px !important;
-    }
-    .s-min-10 {
-        font-size: 12px;
-    }
-</style>
-
