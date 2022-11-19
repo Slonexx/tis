9220be557e4e4c4aa85fb5c2dc2042e24182ce9f
Slonexxx <21moriarty21@mail.ru>
2022-11-19 15:56:25 +0600

    edit

--- app/Http/Controllers/Widget/salesreturnEditController.php
+++ app/Http/Controllers/Widget/salesreturnEditController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers\Widget;
 
+use App\Http\Controllers\BD\getWorkerID;
 use App\Http\Controllers\Config\Lib\VendorApiController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class salesreturnEditController extends Controller
 {
-    public function salesreturn(Request $request){
+    public function salesreturn(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
         $contextKey = $request->contextKey;
         $vendorAPI = new VendorApiController();
         $employee = $vendorAPI->context($contextKey);
         $accountId = $employee->accountId;
 
-        //$Workers = new getWorkerID($employee->id);
+        $Workers = new getWorkerID($employee->id);
+
+        if ($Workers->access == 0 or $Workers->access = null){
+            return view( 'widget.noAccess', [
+                'accountId' => $accountId,
+            ] );
+        }
 
         $entity = 'salesreturn';
 
