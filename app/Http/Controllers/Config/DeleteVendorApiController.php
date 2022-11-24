f54a2b1059353c2c31b5849aea5238d8fb04896a
Slonexxx <21moriarty21@mail.ru>
2022-11-24 18:46:35 +0600

    DEV

--- app/Http/Controllers/Config/DeleteVendorApiController.php
+++ app/Http/Controllers/Config/DeleteVendorApiController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers\Config;
 
+use App\Http\Controllers\BD\getPersonal;
 use App\Http\Controllers\Controller;
+use App\Services\workWithBD\DataBaseService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class DeleteVendorApiController extends Controller
 {
     public function delete($accountId){
+
+        $personal = new getPersonal($accountId);
+        DataBaseService::updatePersonal($personal->accountId, $personal->email, $personal->name, 'деактивированный');
 
         $path = public_path().'/Config/data/'.$accountId.'.json';
         unlink($path);
