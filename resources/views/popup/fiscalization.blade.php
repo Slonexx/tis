f6877da0911eee0d76faded6f4188a0e6b58e5a0
Slonexxx <21moriarty21@mail.ru>
2022-12-30 14:58:33 +0600

    DEV

--- resources/views/popup/fiscalization.blade.php
+++ resources/views/popup/fiscalization.blade.php
@@ -231,6 +231,8 @@
             let object_Quantity = window.document.getElementById('productQuantity_'+id);
             let Quantity = parseInt(object_Quantity.innerText)
             let Discount = parseInt(window.document.getElementById('productQuantity_'+id).innerText);
+            console.log("Discount =")
+            console.log(Discount )
 
             let object_price = window.document.getElementById('productPrice_'+id).innerText;
             let object_Final = window.document.getElementById('productFinal_'+id);
