b8a05d59d119d868a8c2638dbc33f59397d21886
Slonexxx <21moriarty21@mail.ru>
2023-03-31 14:44:45 +0600

    dev

--- resources/views/main/accept.blade.php
+++ resources/views/main/accept.blade.php
@@ -8,7 +8,7 @@
         @include('div.TopServicePartner') <script> NAME_HEADER_TOP_SERVICE("Информация") </script>
 
             <div class="mt-2 alert alert-danger text-center">
-                Данное приложение доступно только если установлен <a href="https://online.moysklad.ru/app/#apps?id=7bd5e7ce-8d4b-4225-9d8a-4a2568690121">Учёт.ТИС</a>
+                Данное приложение доступно только если установлен <a target="_blank" href="https://online.moysklad.ru/app/#apps?id=7bd5e7ce-8d4b-4225-9d8a-4a2568690121">Учёт.ТИС</a>
             </div>
 
     </div>
