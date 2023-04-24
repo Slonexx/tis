9e857e4e925909c4566ad17f6fa7e80f4ee3f8ad
Slonexxx <21moriarty21@mail.ru>
2023-04-24 13:01:20 +0600

    dev

--- app/Services/ticket/TestTicketService.php
+++ app/Services/ticket/TestTicketService.php
@@ -209,9 +209,6 @@ class TestTicketService
 
                     }
                 }
-
-
-                dd($result, $demandPos);
             }
             else {
                 $result[$id] = [
