37b7c6e4876bb164010b49a386e9ee3ee704e875
Slonexxx <21moriarty21@mail.ru>
2023-04-24 15:49:54 +0600

    dev

--- app/Services/ticket/TicketService.php
+++ app/Services/ticket/TicketService.php
@@ -177,6 +177,7 @@ class TicketService
         $result = null;
 
         foreach ($positions as $id => $item){
+            $all = 0;
             $is_nds = trim($item->is_nds, '%');
             $discount = trim($item->discount, '%');
             if ($is_nds == 'без НДС' or $is_nds == "0%"){$is_nds = false;
@@ -203,10 +204,12 @@ class TicketService
                                 'section' => (int) $Setting->idDepartment,
                                 'mark_code' => (string) $code->cis,
                             ];
+                            $all = $all+ $item->price * $item->quantity;
                         }
 
                     }
                 }
+                dd($result, $all);
             }
             else {
                 $result[$id] = [
