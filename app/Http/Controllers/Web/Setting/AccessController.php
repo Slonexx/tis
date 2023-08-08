20124bd24939267e5ef77b3bc467d15ac012165d
Slonexxx <21moriarty21@mail.ru>
2023-08-08 21:54:20 +0600

    CHECK

--- app/Http/Controllers/Web/Setting/AccessController.php
+++ app/Http/Controllers/Web/Setting/AccessController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Web\Setting;
 
 use App\Clients\MsClient;
 use App\Http\Controllers\BD\getAccessByAccountId;
+use App\Http\Controllers\BD\getMainSettingBD;
 use App\Http\Controllers\Config\getSettingVendorController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\getData\getDevices;
 use App\Services\workWithBD\DataBaseService;
+use GuzzleHttp\Exception\BadResponseException;
 use Illuminate\Http\Client\Pool;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -21,50 +23,48 @@ class AccessController extends Controller
 
         $Workers = new getAccessByAccountId($accountId);
 
-        if ( array_key_exists(0, $Workers->access) ){
-            $Workers = null;
+        $Setting = new getMainSettingBD($accountId);
+        $tokenMs = $Setting->tokenMs;
+        if ($Setting->tokenMs == null){
+            return view('setting.no', [
+                'accountId' => $accountId,
+                'isAdmin' => $isAdmin,
+            ]);
+        }
+
+        if ( array_key_exists(0, $Workers->access) ){ $Workers = '';
         } else $Workers = $Workers->access;
 
-        $Setting = new getSettingVendorController($accountId);
-        $tokenMs = $Setting->TokenMoySklad;
+
         $url_employee = 'https://online.moysklad.ru/api/remap/1.2/entity/employee';
-        $Client = new MsClient($tokenMs);
-        $Body_employee = $Client->get($url_employee)->rows;
-        $security = [];
-
-
-        $urls = [];
-        foreach ($Body_employee as $id=>$item){
-            $url_security = $url_employee.'/'.$item->id.'/security';
-            $urls [] = $url_security;
+        try {
+            $Client = new MsClient($tokenMs);
+            $Body_employee = $Client->get($url_employee)->rows;
+        } catch (BadResponseException $e) {
+            return view('setting.error', [
+                'accountId' => $accountId,
+                'isAdmin' => $isAdmin,
+                'message' => $e->getResponse()->getBody()->getContents()
+            ]);
         }
 
-        $pools = function (Pool $pool) use ($urls,$tokenMs){
-            foreach ($urls as $url){
-                $arrPools [] = $pool->withToken($tokenMs)->get($url);
-            }
-            return $arrPools;
-        };
 
-        $responses = Http::pool($pools);
-        $count = 0;
+
         foreach ($Body_employee as $id=>$item){
-            if ( isset($responses[$count]->object()->role) ){
-                $Body_security = $responses[$count]->object()->role;
-                $security[$item->id] = mb_substr ($Body_security->meta->href, 53);
-            } else {
-                $security[$item->id] = 'cashier';
+            $json = $Client->get( $url_employee.'/'.$item->id.'/security');
+            if (property_exists($json, 'role')) {
+                if (mb_substr ($json->role->meta->href, 53)== "cashier") {
+                    unset($Body_employee[$id]);
+                }
             }
-
-            $count++;
         }
+
 
         return view('setting.access', [
             'accountId' => $accountId,
             'isAdmin' => $isAdmin,
             'message'=>$message,
             'employee' => $Body_employee,
-            'security' => $security,
             'workers' => $Workers,
         ]);
     }
@@ -91,10 +91,7 @@ class AccessController extends Controller
             if ($First['accountId'] == null) DataBaseService::createWorker($item['id'], $accountId, $item['access']);
             else DataBaseService::updateWorker($item['id'], $item['access']);
         }
-        $message = [
-            'alert' => ' alert alert-success alert-dismissible fade show in text-center ',
-            'message' => ' Настройки сохранились ',
-        ];
+        $message = ' Настройки сохранились ';
         return redirect()->route('getWorker', [ 'accountId' => $accountId, 'isAdmin' => $isAdmin, 'message'=>$message ]);
     }
 
