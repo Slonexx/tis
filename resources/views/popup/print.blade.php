cac091493336385d91c8164c6b36df641a89c838
Slonexxx <21moriarty21@mail.ru>
2023-09-07 15:40:45 +0600

    daw

--- resources/views/popup/print.blade.php
+++ resources/views/popup/print.blade.php
@@ -1,18 +1,11 @@
-{!! str_replace("!important", "", $html) !!}
-
 <style>
     body {
-        font-family: 'Calibri', 'Arial', sans-serif !important;
-        font-size: 14px !important;
-    }
-    div {
-        font-size: 14px !important;
-    }
-    td {
-        font-size: 13px !important;
+        font-family: 'Times New Roman', 'Arial', sans-serif !important;
     }
 
 </style>
+{!! $html !!}
+
 <script>
     window.print()
 </script>
