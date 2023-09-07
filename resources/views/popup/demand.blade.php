a095e391d1709339a00770044b82b6018e06ff20
Slonexxx <21moriarty21@mail.ru>
2023-09-07 14:31:07 +0600

    daw

--- resources/views/popup/demand.blade.php
+++ resources/views/popup/demand.blade.php
@@ -130,7 +130,7 @@
                 modalShowHide = 'hide'
             }
 
-            if (total-0.01 <= money_card+money_cash){
+            if (parseFloat(total)-0.1 <= parseFloat(money_card) + parseFloat(money_cash)){
                 let url = 'https://smarttis.kz/Popup/demand/send'
 
                 if (modalShowHide === 'show'){
