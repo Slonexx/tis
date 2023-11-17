64d45c847fa2d9333b6418bd6c36602887156212
Slonexxx <21moriarty21@mail.ru>
2023-11-18 00:38:03 +0600

    new adress ms

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,14 @@
 
 use App\Http\Controllers\installOrDeleteController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\Web\Setting\mainController;
 use App\Http\Controllers\WebhookMSController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/ticket',[TicketController::class,'initTicket']);
 
 Route::post('/installOfDelete',[installOrDeleteController::class,'insert']);
-
+Route::get('/get/createAuthToken/{accountId}', [mainController::class, 'createAuthToken']);
 
 
 Route::post('/webhook/customerorder/',[WebhookMSController::class, 'customerorder']);
