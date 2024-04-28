49ec764b09c64463b5abb68b4a3ea731e7a9760b
Slonexx <21moriarty21@mail.ru>
2024-04-28 17:58:22 +0600

    fix

--- public/Config/vendor-endpoint.php
+++ public/Config/vendor-endpoint.php
@@ -6,32 +6,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 
-$url = 'https://smarttis.kz/'.$path;
-$requestBody = file_get_contents('php://input');
-
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => $method,
-    CURLOPT_POSTFIELDS =>$requestBody,
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json'
-    ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-
-/*
 
 $pp = explode('/', $path);
 $n = count($pp);
@@ -75,6 +49,6 @@ if (!$app->getStatusName()) {
 } else if ($replyStatus) {
     header("Content-Type: application/json");
     echo '{"status": "' . $app->getStatusName() . '"}';
-}*/
+}
 
 
